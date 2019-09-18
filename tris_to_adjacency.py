f51295a4a7f8406e3fafa0ece2c2092245ba43c1
kuznesashka <kuznesashka@gmail.com>
2019-09-18 17:55:54 +0300

    Function for transforming triangular grid into the sparse adjacency matrix

---
+++ tris_to_adjacency.py
@@ -0,0 +1,26 @@
+import numpy as np
+from scipy import sparse
+
+
+def tris_to_adjacency(trimesh, n):
+    """Function to transform trimeshs from grid to adjacency matrix
+            Parameters
+            ----------
+            trimesh : numpy.ndarray
+                Triads of vertices from the cortical model
+            n : int
+                Number of sources in model
+
+            Returns
+            -------
+            adj_matrix : adjacency sparse matrix
+            """
+    edge_u = np.concatenate((trimesh[:, 0], trimesh[:, 0], trimesh[:, 1]))
+    edge_v = np.concatenate((trimesh[:, 1], trimesh[:, 2], trimesh[:, 2]))
+
+    # adjacency matrix - initialize with 0
+    # see also: http://stackoverflow.com/questions/6667201/how-to-define-two-dimensional-array-in-python
+    adj_matrix = sparse.csr_matrix((n, n))
+
+    adj_matrix[edge_u, edge_v] = 1
+    return adj_matrix
