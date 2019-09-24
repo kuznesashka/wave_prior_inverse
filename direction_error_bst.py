802d4e897176546e3877330f772762ff86cbfe0d
kuznesashka <kuznesashka@gmail.com>
2019-09-24 12:29:10 +0300

    Loop by spatial jitter value

--- direction_error_bst.py
+++ direction_error_bst.py
@@ -1,4 +1,5 @@
 from create_waves_on_sensors import create_waves_on_sensors
+from create_blob_on_sensors import create_blob_on_sensors
 from generate_brain_noise import generate_brain_noise
 from LASSO_inverse_solve import LASSO_inverse_solve
 import scipy.io
@@ -41,6 +42,7 @@ def direction_error_bst(data_dir, channel_type, params, snr, spatial_jitter, num
     cortex_smooth_dense_raw = scipy.io.loadmat(data_dir + '/cortex_smooth_medium.mat')
     G_raw = scipy.io.loadmat(data_dir + '/G.mat')
     cortex_raw = scipy.io.loadmat(data_dir + '/cortex.mat')
+    cortex_smooth_raw = scipy.io.loadmat(data_dir + '/cortex_smooth.mat')
 
     # pick the appropriate channels
     if channel_type == 'mag':
@@ -53,59 +55,70 @@ def direction_error_bst(data_dir, channel_type, params, snr, spatial_jitter, num
         print('Wrong channel name')
 
     cortex_dense = cortex_dense_raw['cortex'][0]
+    cortex = cortex_raw['cortex'][0]
+    cortex_smooth = cortex_smooth_raw['cortex_smooth'][0]
     cortex_smooth_dense = cortex_smooth_dense_raw['cortex'][0]
+
+    vertices = cortex[0][1]
     vertices_dense = cortex_dense[0][1]
+    vertices_dense_smooth = cortex_smooth_dense[0][0]
+    assert (vertices_dense.shape == vertices_dense_smooth.shape)
+    faces_dense = cortex_dense[0][0]-1
+    assert(faces_dense.shape[1] == 3)
+    assert (faces_dense.shape[0] > vertices_dense.shape[0])
 
     # Visualization of 3d brain surfaces
     # triangular_mesh(vertices[:, 0], vertices[:, 1], vertices[:, 2], faces, color=(0, 1, 1))
 
     # 2. PARAMETERS
     # TODO: fix duration
-    speeds = params['speeds']  # speed range
+    # speeds = params['speeds']  # speed range
     T = int(params['duration']*params['Fs']+1)*2  # duration in time
 
-    speed_generated = np.zeros([len(snr), len(spatial_jitter), num_sim], dtype=int)
-    speed_estimated = np.zeros([len(snr), len(spatial_jitter), num_sim], dtype=int)
-    direction_error = np.zeros([len(snr), len(spatial_jitter), num_sim])
-    direction_error_smooth = np.zeros([len(snr), len(spatial_jitter), num_sim])
-    direction_error_pca = np.zeros([len(snr), len(spatial_jitter), num_sim])
-
-    # direction_error = np.zeros([len(snr), num_sim])  # error in direction detection
+    speed_generated = np.zeros([len(spatial_jitter), len(snr), num_sim], dtype=int)
+    speed_estimated = np.zeros([len(spatial_jitter), len(snr), num_sim], dtype=int)
+    direction_error = np.zeros([len(spatial_jitter), len(snr), num_sim])
+    direction_error_smooth = np.zeros([len(spatial_jitter), len(snr), num_sim])
+    direction_error_pca = np.zeros([len(spatial_jitter), len(snr), num_sim])
 
     # 3. ROC CURVE
-    # y_true = np.zeros(num_sim * 2)  # true labels
-    # y_true[0:num_sim] = np.ones(num_sim)
-    # auc = np.zeros(len(snr))
-    #
-    # # ROC figure
-    # plt.figure()
-    # lw = 2
-    # plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-    # plt.xlim([0.0, 1.0])
-    # plt.ylim([0.0, 1.05])
-    # plt.xlabel('False Positive Rate')
-    # plt.ylabel('True Positive Rate')
+    y_true = np.zeros(num_sim * 2)  # true labels
+    y_true[0:num_sim] = np.ones(num_sim)
+    auc = np.zeros([len(snr), len(spatial_jitter)])
 
-    for i in range(0, len(snr)):
-        for j in range(0, len(spatial_jitter)):
+    # ROC figure
+    plt.figure()
+
+    for i in range(0, len(spatial_jitter)):
+        plt.subplot(1, len(spatial_jitter), (i+1))
+        lw = 2
+        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+
+        for j in range(0, len(snr)):
             score_fit = np.zeros(2*num_sim)  # R-squared metrics for all simulations
             generate_direction = np.zeros([num_sim, 3])  # true directions modeled
             generate_direction_smooth = np.zeros([num_sim, 3])  # true directions modeled
             generate_direction_pca = np.zeros([num_sim, 3])  # true directions modeled
-            # src_idx = np.zeros(num_sim, dtype=int)  # assumed starting source from the sparse model
+            src_idx = np.zeros(num_sim, dtype=int)  # assumed starting source from the sparse model
             src_idx_dense = np.zeros(num_sim, dtype=int)  # true starting source from the dense model
             brain_noise_norm = np.zeros([G_dense.shape[0], T, num_sim])  # brain noise array
 
             # in first num_sim trials the data is generated using traveling waves
             for sim_n in range(0, num_sim):
-                src_idx_dense[sim_n] = np.random.randint(0, G_dense.shape[1])  # random starting source from sparse cortical model
+                src_idx[sim_n] = np.random.randint(0, G.shape[1])  # random starting source from sparse cortical model
                 # compute distance from starting source to all sources from dense cortical model
-                # dist = np.sum(np.sqrt((np.repeat(vertices[src_idx[sim_n], np.newaxis],
-                #                                  vertices_dense.shape[0], axis=0) - vertices_dense)**2), axis=1)
-                # # find close sources
-                # ind_close = np.where((dist > 0.002)&(dist <= 0.005))[0]
-                # src_idx_dense[sim_n] = ind_close[np.random.randint(0, len(ind_close))]  # pick randomly new starting source
-                [sensor_waves, direction, direction_smooth, direction_pca] = create_waves_on_sensors(cortex_dense, cortex_smooth_dense, params,
+                dist = np.sum(np.sqrt((np.repeat(vertices[src_idx[sim_n], np.newaxis],
+                                                 vertices_dense.shape[0], axis=0) - vertices_dense)**2), axis=1)
+                # dist_smooth = np.sum(np.sqrt((np.repeat(vertices_dense_smooth[src_idx_dense[sim_n], np.newaxis],
+                #                                  vertices_dense_smooth.shape[0], axis=0) - vertices_dense_smooth) ** 2), axis=1)
+                # find close sources
+                ind_close = np.where((dist > spatial_jitter[i])&(dist <= (spatial_jitter[i]+0.002)))[0]
+                src_idx_dense[sim_n] = ind_close[np.random.randint(0, len(ind_close))]  # pick randomly new starting source
+                [sensor_waves, path_final_gen, path_final_smooth_gen, direction, direction_smooth, direction_pca] = create_waves_on_sensors(cortex_dense, cortex_smooth_dense, params,
                                                                             G_dense, src_idx_dense[sim_n], spherical=False)
                 speed_generated[i, j, sim_n] = np.random.randint(0, sensor_waves.shape[1])  # speed for wave simulation
                 direction_ind = np.random.randint(0, sensor_waves.shape[0])
@@ -118,7 +131,7 @@ def direction_error_bst(data_dir, channel_type, params, snr, spatial_jitter, num
                                             np.linalg.norm(brain_noise[:, :sensor_waves.shape[3]])  # normalized
                 wave_picked = sensor_waves[direction_ind, speed_generated[i, j, sim_n], :, :]
                 wave_picked_norm = wave_picked/np.linalg.norm(wave_picked)  # normalized wave
-                data = snr[i]*wave_picked_norm + brain_noise_norm[:, :, sim_n]  # wave + noise
+                data = snr[j]*wave_picked_norm + brain_noise_norm[:, :, sim_n]  # wave + noise
 
                 # plt.figure()
                 # plt.plot(np.concatenate((brain_noise_norm[:, :, sim_n], wave_picked_norm), axis=1).T)
@@ -127,52 +140,78 @@ def direction_error_bst(data_dir, channel_type, params, snr, spatial_jitter, num
 
                 # Generate basis waves using sparse cortical model starting from the initially picked point
                 # (with spatial error)
-                [sensor_waves, direction, direction_smooth, direction_pca] = create_waves_on_sensors(cortex_dense, cortex_smooth_dense, params, G_dense,
-                                                                                   src_idx_dense[sim_n], spherical=False)
+                [sensor_waves, path_final, path_final_smooth, direction, direction_smooth, direction_pca] = create_waves_on_sensors(cortex, cortex_smooth, params, G,
+                                                                                   src_idx[sim_n], spherical=False)
                 # Solve the LASSO problem without intercept
                 [score_fit[sim_n], best_intercept, best_coefs, best_shift, speed_estimated[i, j, sim_n]] = \
                         LASSO_inverse_solve(data, sensor_waves, False)
+                direction_ind_estimated = np.argmax(best_coefs)
+
+                # Visualization of 3d brain surfaces
+                # triangular_mesh(vertices_dense[:, 0], vertices_dense[:, 1], vertices_dense[:, 2], faces_dense, color=(0, 1, 1))
+
+                # ind_close = np.where(dist_smooth <= 0.03)[0]
+                # fig = plt.figure()
+                # ax = fig.add_subplot(111, projection='3d')
+                # ax.scatter(vertices_dense_smooth[ind_close, 0], vertices_dense_smooth[ind_close, 1], vertices_dense_smooth[ind_close, 2])
+                # ax.scatter(path_final_smooth_gen[direction_ind, speed_generated[i, j, sim_n], :, 0],
+                #            path_final_smooth_gen[direction_ind, speed_generated[i, j, sim_n], :, 1],
+                #            path_final_smooth_gen[direction_ind, speed_generated[i, j, sim_n], :, 2], marker='^')
+                # ax.scatter(path_final_smooth[direction_ind_estimated, speed_estimated[i, j, sim_n], :, 0],
+                #            path_final_smooth[direction_ind_estimated, speed_estimated[i, j, sim_n], :, 1],
+                #            path_final_smooth[direction_ind_estimated, speed_estimated[i, j, sim_n], :, 2], marker='^')
+                #
+                # ind_close = np.where(dist <= 0.03)[0]
+                # fig = plt.figure()
+                # ax = fig.add_subplot(111, projection='3d')
+                # ax.scatter(vertices_dense[ind_close, 0], vertices_dense[ind_close, 1], vertices_dense[ind_close, 2])
+                # ax.scatter(path_final_gen[direction_ind, speed_generated[i, j, sim_n], :, 0],
+                #            path_final_gen[direction_ind, speed_generated[i, j, sim_n], :, 1],
+                #            path_final_gen[direction_ind, speed_generated[i, j, sim_n], :, 2], marker='^')
+                # ax.scatter(path_final[direction_ind_estimated, speed_estimated[i, j, sim_n], :, 0],
+                #            path_final[direction_ind_estimated, speed_estimated[i, j, sim_n], :, 1],
+                #            path_final[direction_ind_estimated, speed_estimated[i, j, sim_n], :, 2], marker='^')
 
                 # error in direction predicted (out of 1)
-                direction_error[i, j, sim_n] = 1 - abs(direction[np.argmax(best_coefs), speed_generated[i, j, sim_n], :] @ \
+                direction_error[i, j, sim_n] = 1 - abs(direction[direction_ind_estimated, speed_estimated[i, j, sim_n], :] @ \
                                             generate_direction[sim_n, :] / \
-                                            np.linalg.norm(direction[np.argmax(best_coefs), speed_estimated[i, j, sim_n], :]) / \
+                                            np.linalg.norm(direction[direction_ind_estimated, speed_estimated[i, j, sim_n], :]) / \
                                             np.linalg.norm(generate_direction[sim_n, :]))
-                direction_error_smooth[i, j, sim_n] = 1 - abs(direction_smooth[np.argmax(best_coefs), speed_generated[i, j, sim_n], :] @ \
-                                               generate_direction_smooth[sim_n, :] / np.linalg.norm(direction_smooth[np.argmax(best_coefs),
+                direction_error_smooth[i, j, sim_n] = 1 - abs(direction_smooth[direction_ind_estimated, speed_estimated[i, j, sim_n], :] @ \
+                                               generate_direction_smooth[sim_n, :] / np.linalg.norm(direction_smooth[direction_ind_estimated,
                                                 speed_estimated[i, j, sim_n], :]) / np.linalg.norm(generate_direction_smooth[sim_n, :]))
-                direction_error_pca[i, j, sim_n] = 1 - abs(direction_pca[np.argmax(best_coefs), speed_generated[i, j, sim_n], :] @ \
+                direction_error_pca[i, j, sim_n] = 1 - abs(direction_pca[direction_ind_estimated, speed_estimated[i, j, sim_n], :] @ \
                                                generate_direction_pca[sim_n, :] / np.linalg.norm(direction_pca[np.argmax(best_coefs), speed_estimated[i, j, sim_n], :]) / \
                                                np.linalg.norm(generate_direction_pca[sim_n, :]))
                 print(i, j, sim_n)
 
-        # next num_sim trials without waves, only with static oscillating blobs
-    #     for sim_n in range(num_sim, 2*num_sim):
-    #         idx_dense = src_idx_dense[sim_n - num_sim]
-    #         idx = src_idx[sim_n - num_sim]
-    #         [sensor_blob, path_indices] = create_blob_on_sensors(cortex_dense, params, G_dense, idx_dense, T)
-    #         [sensor_waves, direction, path_final] = create_waves_on_sensors(cortex, params, G, idx, spherical=False)
-    #
-    #         brain_noise = brain_noise_norm[:, :, sim_n-num_sim]
-    #         sensor_blob_norm = sensor_blob/np.linalg.norm(sensor_blob)
-    #         data = snr_level*sensor_blob_norm + brain_noise
-    #
-    #         # plt.figure()
-    #         # plt.plot(data.T)
-    #
-    #         [score_fit[sim_n], best_intercept[sim_n], best_coefs, best_shift, best_speed_ind] = \
-    #             LASSO_inverse_solve(data, sensor_waves, False)
-    #         print(sim_n)
-    #
-    #     y_score = score_fit
-    #     fpr, tpr, thresholds = metrics.roc_curve(y_true, y_score)
-    #     auc[k] = metrics.roc_auc_score(y_true, y_score)
-    #     plt.plot(fpr, tpr, lw=lw, label='ROC curve for SNR {0}, (area = {1:0.2f})'.format(snr_level, auc[k]))
-    #     k += 1
-    #
-    # plt.title('Receiver operating characteristics for different SNR')
-    # plt.legend(loc="lower right")
-    # plt.show()
+            # next num_sim trials without waves, only with static oscillating blobs
+            for sim_n in range(num_sim, 2*num_sim):
+                idx_dense = src_idx_dense[sim_n - num_sim]
+                idx = src_idx[sim_n - num_sim]
+                [sensor_blob, path_indices] = create_blob_on_sensors(cortex_dense, params, G_dense, idx_dense, T)
+                [sensor_waves, path_final, path_final_smooth, direction, direction_smooth, direction_pca] = \
+                    create_waves_on_sensors(cortex, cortex_smooth, params, G, idx, spherical=False)
+
+                brain_noise = brain_noise_norm[:, :, sim_n-num_sim]
+                sensor_blob_norm = sensor_blob/np.linalg.norm(sensor_blob)
+                data = snr[i]*sensor_blob_norm + brain_noise
+
+                # plt.figure()
+                # plt.plot(data.T)
+
+                [score_fit[sim_n], best_intercept, best_coefs, best_shift, best_speed_ind] = \
+                    LASSO_inverse_solve(data, sensor_waves, False)
+                print(sim_n)
+
+            y_score = score_fit
+            fpr, tpr, thresholds = metrics.roc_curve(y_true, y_score)
+            auc[i, j] = metrics.roc_auc_score(y_true, y_score)
+            plt.plot(fpr, tpr, lw=lw, label='ROC curve for SNR {0}, (area = {1:0.2f})'.format(snr[j], auc[i, j]))
+
+        plt.title('Receiver operating characteristics for different SNR')
+        plt.legend(loc="lower right")
+        plt.show()
 
     plt.figure()
     plt.subplot(1, 3, 1)
@@ -193,7 +232,6 @@ def direction_error_bst(data_dir, channel_type, params, snr, spatial_jitter, num
     plt.xlabel('SNR')
     plt.ylim([0, 1])
     plt.ylabel('Error between detected and generated speeds in m/s')
-
 
     for j in range(0, len(spatial_jitter)):
         for s in range(0, speed_generated.shape[0]):
