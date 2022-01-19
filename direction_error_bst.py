2bbfbd6fa0b34b630c41d661f68ca67f353cd624
Aleksandra Kuznetsova <kuznesashka@gmail.com>
2022-01-20 00:42:50 +0300

    Refactor

--- direction_error_bst.py
+++ direction_error_bst.py
@@ -14,118 +14,130 @@ from LASSO_inverse_solve import LASSO_inverse_solve
 def direction_error_bst(
     data_dir: str,
     channel_type: str,
-    params: Dict[str, Any],
-    snr: List[float],
-    spatial_jitter: List[float],
-    num_sim: int = 100,
+    wave_generation_params: Dict[str, Any],
+    snr_list: List[float],
+    spatial_jitter_list: List[float],
+    simulation_number: int = 100,
 ):
-    """Function to run Monte Carlo simulations
-    Focus on speed and direction error: as a function of spatial jitter, snr
-    All models and forward operators are generated in brainstorm
-        Parameters
-        ----------
-        data_dir : str
-            Data directory with G.mat and cortex.mat
-        channel_type : str
-            Type of channels used 'grad' or 'mag'
-        params : Dict[str, Any]
-            Wave modeling parameters
-        snr : List[float]
-            List with all considered snr values
-        spatial_jitter : List[float]
-            List of lower bounds of spatial jitter values (in m) with spatial jitter distributed uniformly
-            between [jitter[i], jitter[i]+0.003]
-        num_sim : int
-            Number of simulations for one class
-        Returns
-        -------
-        auc : AUC values for all snr levels
-        speed_error : difference between simulated and detected speeds in m/s
-        direction_error : 1-correlation between generated and detected directions
-        ROC curve plot, Error plots
+    """Monte Carlo simulations.
+    Function calculates speed and direction error as a function of spatial jitter and SNR.
+    All cortical models and forward operators are calculated in Brainstorm.
+
+    Parameters
+    ----------
+    data_dir : str
+        Data directory with precalculated in Brainstorm:
+        - G.mat (forward operator, low source number)
+        - cortex.mat (cortical model, low source number)
+        - G_dense.mat (forward_operator, high source number)
+    channel_type : str
+        MEG channels to use: 'grad' or 'mag'.
+    wave_generation_params : Dict[str, Any]
+        Wave modeling parameters.
+    snr_list : List[float]
+        List of SNR values to consider.
+    spatial_jitter_list : List[float]
+        List of lower bound values (in m) for spatial jitter. Spatial jitter is distributed uniformly
+        between [jitter[i], jitter[i] + 0.003].
+    simulation_number : int
+        Simulation number for one class.
+
+    Returns
+    -------
+    auc :
+        AUC values for all SNR levels.
+    speed_error :
+        Difference between simulated and detected speeds in m/s.
+    direction_error :
+        1 - correlation between generated and detected directions.
+
+    ROC curve plot
+    Error plots
     """
 
-    # 1. UPLOADING FORWARD OPERATOR AND CORTICAL MODEL FROM BST
-    # uploading sparse and dense forward model and cortical model
-    G_dense_raw = scipy.io.loadmat(data_dir + "/G_low.mat")
-    cortex_dense_raw = scipy.io.loadmat(data_dir + "/cortex_low.mat")
-    cortex_smooth_dense_raw = scipy.io.loadmat(data_dir + "/cortex_smooth_low.mat")
+    # Dense cortical grid
+    G_dense_raw = scipy.io.loadmat(data_dir + "/G_dense.mat")
+    cortex_dense_raw = scipy.io.loadmat(data_dir + "/cortex_dense.mat")
+    cortex_smooth_dense_raw = scipy.io.loadmat(data_dir + "/cortex_smooth_dense.mat")
+
+    # Sparse cortical grid
     G_raw = scipy.io.loadmat(data_dir + "/G.mat")
     cortex_raw = scipy.io.loadmat(data_dir + "/cortex.mat")
     cortex_smooth_raw = scipy.io.loadmat(data_dir + "/cortex_smooth.mat")
 
-    # pick the appropriate channels
+    # Select channels according to channel_type
     if channel_type == "mag":
-        G_dense = G_dense_raw["G"][np.arange(2, 306, 3)]  # magnetometers
-        G = G_raw["G"][np.arange(2, 306, 3)]  # magnetometers
+        mag_indices = np.arange(2, 306, 3)
+        G_dense = G_dense_raw["G_dense_raw"][mag_indices]
+        G = G_raw["G_raw"][mag_indices]
     elif channel_type == "grad":
-        G_dense = G_dense_raw["G"][
-            np.setdiff1d(range(0, 306), np.arange(2, 306, 3))
-        ]  # gradiometers
-        G = G_raw["G"][
-            np.setdiff1d(range(0, 306), np.arange(2, 306, 3))
-        ]  # magnetometers
+        grad_indices = np.setdiff1d(range(0, 306), np.arange(2, 306, 3))
+        G_dense = G_dense_raw["G_dense_raw"][grad_indices]
+        G = G_raw["G_raw"][grad_indices]
     else:
         print("Wrong channel name")
 
-    cortex_dense = cortex_dense_raw["cortex"][0]
-    cortex = cortex_raw["cortex"][0]
-    cortex_smooth = cortex_smooth_raw["cortex_smooth"][0]
-    cortex_smooth_dense = cortex_smooth_dense_raw["cortex_smooth"][0]
+    cortex_dense = cortex_dense_raw["cortex_dense_raw"][0]
+    cortex = cortex_raw["cortex_raw"][0]
+    cortex_smooth = cortex_smooth_raw["cortex_smooth_raw"][0]
+    cortex_smooth_dense = cortex_smooth_dense_raw["cortex_smooth_dense_raw"][0]
 
     vertices = cortex[0][1]
     vertices_dense = cortex_dense[0][1]
     vertices_dense_smooth = cortex_smooth_dense[0][0]
-    assert vertices_dense.shape == vertices_dense_smooth.shape
     faces_dense = cortex_dense[0][0] - 1
+
+    assert vertices_dense.shape == vertices_dense_smooth.shape
     assert faces_dense.shape[1] == 3
     assert faces_dense.shape[0] > vertices_dense.shape[0]
 
-    # 2. PARAMETERS
-    T = int(params["duration"] * params["Fs"] + 1) * 2  # duration in time
+    T = int(wave_generation_params["duration"] * wave_generation_params["Fs"] + 1) * 2  # duration in time
 
-    speed_generated = np.zeros([len(spatial_jitter), len(snr), num_sim], dtype=int)
-    speed_estimated = np.zeros([len(spatial_jitter), len(snr), num_sim], dtype=int)
-    direction_error = np.zeros([len(spatial_jitter), len(snr), num_sim])
-    direction_error_smooth = np.zeros([len(spatial_jitter), len(snr), num_sim])
-    direction_error_pca = np.zeros([len(spatial_jitter), len(snr), num_sim])
-    spatial_error = np.zeros([len(spatial_jitter), len(snr), num_sim])
+    spatial_jitter_num = len(spatial_jitter_list)
+    snr_num = len(snr_list)
+    
+    speed_generated = np.zeros([spatial_jitter_num, snr_num, simulation_number], dtype=int)
+    speed_estimated = np.zeros([spatial_jitter_num, snr_num, simulation_number], dtype=int)
+    direction_error = np.zeros([spatial_jitter_num, snr_num, simulation_number])
+    direction_error_smooth = np.zeros([spatial_jitter_num, snr_num, simulation_number])
+    direction_error_pca = np.zeros([spatial_jitter_num, snr_num, simulation_number])
+    spatial_error = np.zeros([spatial_jitter_num, snr_num, simulation_number])
 
     # 3. ROC CURVE
-    y_true = np.zeros(num_sim * 2)  # true labels
-    y_true[0:num_sim] = np.ones(num_sim)
-    auc = np.zeros([len(snr), len(spatial_jitter)])
+    y_true = np.zeros(simulation_number * 2)  # true labels
+    y_true[0:simulation_number] = np.ones(simulation_number)
+    auc = np.zeros([snr_num, spatial_jitter_num])
 
     # ROC figure
     plt.figure()
 
-    for i in range(0, len(spatial_jitter)):
-        plt.subplot(1, len(spatial_jitter), (i + 1))
+    for i in range(0, spatial_jitter_num):
+        plt.subplot(1, spatial_jitter_num, (i + 1))
         plt.plot([0, 1], [0, 1], color="navy", lw=2, linestyle="--")
         plt.xlim([0.0, 1.0])
         plt.ylim([0.0, 1.05])
         plt.xlabel("False Positive Rate")
         plt.ylabel("True Positive Rate")
 
-        for j in range(0, len(snr)):
-            score_fit = np.zeros(2 * num_sim)  # R-squared metrics for all simulations
+        for j in range(0, snr_num):
+            score_fit = np.zeros(2 * simulation_number)  # R-squared metrics for all simulations
 
             # true directions modeled
-            generate_direction = np.zeros([num_sim, 3])
-            generate_direction_smooth = np.zeros([num_sim, 3])
-            generate_direction_pca = np.zeros([num_sim, 3])
+            generate_direction = np.zeros([simulation_number, 3])
+            generate_direction_smooth = np.zeros([simulation_number, 3])
+            generate_direction_pca = np.zeros([simulation_number, 3])
 
             # assumed starting source from the sparse model
-            src_idx = np.zeros(num_sim, dtype=int)
+            src_idx = np.zeros(simulation_number, dtype=int)
 
             # true starting source from the dense model
-            src_idx_dense = np.zeros(num_sim, dtype=int)
+            src_idx_dense = np.zeros(simulation_number, dtype=int)
 
             # brain noise array
-            brain_noise_norm = np.zeros([G_dense.shape[0], T, num_sim])
+            brain_noise_norm = np.zeros([G_dense.shape[0], T, simulation_number])
 
-            # in first num_sim trials the data is generated using traveling waves
-            for sim_n in range(0, num_sim):
+            # in first simulation_number trials the data is generated using traveling waves
+            for sim_n in range(0, simulation_number):
                 # random starting source from sparse cortical model
                 src_idx[sim_n] = np.random.randint(0, G.shape[1])
 
@@ -147,7 +159,7 @@ def direction_error_bst(
 
                 # find close sources
                 ind_close = np.where(
-                    (dist > spatial_jitter[i]) & (dist <= (spatial_jitter[i] + 0.003))
+                    (dist > spatial_jitter_list[i]) & (dist <= (spatial_jitter_list[i] + 0.003))
                 )[0]
                 if len(ind_close) == 0:
                     continue
@@ -168,7 +180,7 @@ def direction_error_bst(
                 ] = create_waves_on_sensors(
                     cortex=cortex_dense,
                     cortex_smooth=cortex_smooth_dense,
-                    params=params,
+                    params=wave_generation_params,
                     G=G_dense,
                     start_point=src_idx_dense[sim_n],
                     spherical=False
@@ -205,7 +217,7 @@ def direction_error_bst(
                 wave_picked_norm = wave_picked / np.linalg.norm(wave_picked)
 
                 # wave + noise
-                data = snr[j] * wave_picked_norm + brain_noise_norm[:, :, sim_n]
+                data = snr_list[j] * wave_picked_norm + brain_noise_norm[:, :, sim_n]
 
                 # Generate basis waves using sparse cortical model starting from the initially picked point
                 # (with spatial error)
@@ -219,7 +231,7 @@ def direction_error_bst(
                 ] = create_waves_on_sensors(
                     cortex=cortex,
                     cortex_smooth=cortex_smooth,
-                    params=params,
+                    params=wave_generation_params,
                     G=G,
                     start_point=src_idx[sim_n],
                     spherical=False
@@ -276,13 +288,13 @@ def direction_error_bst(
 
                 print(i, j, sim_n)
 
-            # next num_sim trials without waves, only with static oscillating blobs
-            for sim_n in range(num_sim, 2 * num_sim):
-                idx_dense = src_idx_dense[sim_n - num_sim]
-                idx = src_idx[sim_n - num_sim]
+            # next simulation_number trials without waves, only with static oscillating blobs
+            for sim_n in range(simulation_number, 2 * simulation_number):
+                idx_dense = src_idx_dense[sim_n - simulation_number]
+                idx = src_idx[sim_n - simulation_number]
                 [sensor_blob, _] = create_blob_on_sensors(
                     cortex=cortex_dense,
-                    params=params,
+                    params=wave_generation_params,
                     G=G_dense,
                     start_point=idx_dense,
                     T=T
@@ -291,15 +303,15 @@ def direction_error_bst(
                 [sensor_waves, _] = create_waves_on_sensors(
                     cortex=cortex,
                     cortex_smooth=cortex_smooth,
-                    params=params,
+                    params=wave_generation_params,
                     G=G,
                     start_point=idx,
                     spherical=False
                 )
 
-                brain_noise = brain_noise_norm[:, :, sim_n - num_sim]
+                brain_noise = brain_noise_norm[:, :, sim_n - simulation_number]
                 sensor_blob_norm = sensor_blob / np.linalg.norm(sensor_blob)
-                data = snr[i] * sensor_blob_norm + brain_noise
+                data = snr_list[i] * sensor_blob_norm + brain_noise
 
                 [score_fit[sim_n], _] = LASSO_inverse_solve(data=data, waves=sensor_waves, fit_intercept=False)
                 print(sim_n)
@@ -312,13 +324,13 @@ def direction_error_bst(
                 tpr,
                 lw=2,
                 label="ROC curve for SNR {0}, (area = {1:0.2f})".format(
-                    snr[j], auc[i, j]
+                    snr_list[j], auc[i, j]
                 ),
             )
 
         plt.title(
             "Mean spatial jitter = "
-            + str((spatial_jitter[i] + 0.003 / 2) * 1000)
+            + str((spatial_jitter_list[i] + 0.003 / 2) * 1000)
             + " mm"
         )
         plt.legend(loc="lower right")
@@ -326,37 +338,37 @@ def direction_error_bst(
 
     plt.figure()
     plt.subplot(1, 3, 1)
-    for j in range(0, len(spatial_jitter)):
-        plt.plot(snr, np.mean(direction_error[j, :, :], axis=1), "o-")
+    for j in range(0, spatial_jitter_num):
+        plt.plot(snr_list, np.mean(direction_error[j, :, :], axis=1), "o-")
         plt.title("Direction detection error (with curvature)")
         plt.ylim([0, 1])
         plt.xlabel("SNR")
         plt.ylabel("1 - correlation between generated and detected")
-    plt.legend((np.array(spatial_jitter) + 0.0015) * 1000)
+    plt.legend((np.array(spatial_jitter_list) + 0.0015) * 1000)
     plt.subplot(1, 3, 2)
-    for j in range(0, len(spatial_jitter)):
-        plt.plot(snr, np.mean(direction_error_smooth[j, :, :], axis=1), "o-")
+    for j in range(0, spatial_jitter_num):
+        plt.plot(snr_list, np.mean(direction_error_smooth[j, :, :], axis=1), "o-")
         plt.title("Direction detection error (on smooth cortex)")
         plt.ylim([0, 1])
         plt.xlabel("SNR")
         plt.ylabel("1 - correlation between generated and detected")
-    plt.legend((np.array(spatial_jitter) + 0.0015) * 1000)
+    plt.legend((np.array(spatial_jitter_list) + 0.0015) * 1000)
     plt.subplot(1, 3, 3)
-    for j in range(0, len(spatial_jitter)):
-        plt.plot(snr, np.mean(direction_error_pca[j, :, :], axis=1), "o-")
+    for j in range(0, spatial_jitter_num):
+        plt.plot(snr_list, np.mean(direction_error_pca[j, :, :], axis=1), "o-")
         plt.title("Direction detection error (main direction)")
         plt.xlabel("SNR")
         plt.ylim([0, 1])
         plt.ylabel("Error between detected and generated speeds in m/s")
-    plt.legend((np.array(spatial_jitter) + 0.0015) * 1000)
+    plt.legend((np.array(spatial_jitter_list) + 0.0015) * 1000)
 
     plt.figure()
     k = 1
-    for j in range(0, len(spatial_jitter)):
+    for j in range(0, spatial_jitter_num):
         for s in range(0, speed_generated.shape[0]):
             jitter_1 = 0.2 * np.random.rand(speed_estimated.shape[2])
             jitter_2 = 0.2 * np.random.rand(speed_estimated.shape[2])
-            plt.subplot(len(spatial_jitter), len(snr), k)
+            plt.subplot(spatial_jitter_num, snr_num, k)
             plt.scatter(
                 speed_generated[j, s, :] + jitter_1, speed_estimated[j, s, :] + jitter_2
             )
@@ -365,13 +377,13 @@ def direction_error_bst(
             plt.plot(np.arange(-1, 10, 0.1), np.arange(-1, 10, 0.1), "--")
             plt.xlim([-1, 10])
             plt.ylim([-1, 10])
-            plt.xticks(range(0, 10), params["speeds"])  # Set locations and labels
-            plt.yticks(range(0, 10), params["speeds"])  # Set locations and labels
+            plt.xticks(range(0, 10), wave_generation_params["speeds"])  # Set locations and labels
+            plt.yticks(range(0, 10), wave_generation_params["speeds"])  # Set locations and labels
             plt.title(
                 "SNR level = "
-                + str(snr[s])
+                + str(snr_list[s])
                 + ", Mean spatial jitter = "
-                + str((spatial_jitter[j] + 0.0015) * 1000)
+                + str((spatial_jitter_list[j] + 0.0015) * 1000)
                 + " mm"
             )
             k += 1
--- main.py
+++ main.py
@@ -1,25 +1,53 @@
 from simulations_roc import simulations
 from direction_error_bst import direction_error_bst
 
-# Simulations
-channel_type = "grad"
-data_dir = "/home/ksasha/PycharmProjects/Wave_prior_inverse"
+from typing import List, Dict, Any
 
-# wave generation parameters
-params = {
+
+wave_generation_params = {
     "duration": 0.02,
     "Fs": 1000,
     "speeds": [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8],
 }
-num_sim = 100
-snr = [2]
-spatial_jitter = [0]
 
-[
-    auc,
-    direction_error,
-    direction_error_smooth,
-    direction_error_pca,
-    speed_generated,
-    speed_estimated,
-] = direction_error_bst(data_dir, channel_type, params, snr, spatial_jitter, num_sim)
+
+def create_simulations_for_paper(
+        data_dir: str,
+        wave_generation_params: Dict[str, Any],
+        snr_list: List[float],
+        spatial_jitter_list: List[float],
+        channel_type: str = "grad",
+        simulation_number: int = 100
+):
+    """
+    simulations from Fig. 3 (https://www.biorxiv.org/content/10.1101/2020.05.17.101121v1.full.pdf)
+
+    Returns
+    -------
+
+    """
+
+    [
+        auc,
+        direction_error,
+        direction_error_smooth,
+        direction_error_pca,
+        speed_generated,
+        speed_estimated,
+    ] = direction_error_bst(
+        data_dir=data_dir,
+        channel_type=channel_type,
+        wave_generation_params=wave_generation_params,
+        snr_list=snr_list,
+        spatial_jitter_list=spatial_jitter_list,
+        simulation_number=simulation_number
+    )
+
+    return (
+        auc,
+        direction_error,
+        direction_error_smooth,
+        direction_error_pca,
+        speed_generated,
+        speed_estimated
+    )
